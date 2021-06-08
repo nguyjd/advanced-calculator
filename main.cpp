60ff644cc5d83220149c103155410ab894d80d3d
nguyjd <nguyjd@gmail.com>
2021-06-08 14:31:53 -0400

    Added a Factorial function to test the multiplication

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include "main.h"
 
+// Recursion function to calulate the factorial.
+// The function loops until n == 0
+BigInteger FindFactorial(const int n)
+{
+
+	// Base Condition.
+	if (n == 0)
+	{
+
+		return 1;
+
+	}
+
+	// This multiply the n * n-1
+	return static_cast<BigInteger>(n) * FindFactorial(n - 1);
+
+}
+
 int main()
 {
 
@@ -25,10 +43,15 @@ int main()
 	//test.TestingBigIntGreaterThanComparisonOperator(500000, false);
 	//test.TestingBigIntLessThanOrEqualComparisonOperator(100000, false);
 	//test.TestingBigIntGreaterThanOrEqualComparisonOperator(100000, false);
-	test.TestingBigIntLongMultiplication(100000, true);
+	//test.TestingBigIntLongMultiplication(100000, true);
+	test.TestingBigIntMathTiming(10000);
 	
+	const int numFact = 500;
+	BigInteger fact = FindFactorial(numFact);
 	
-	
+	std::cout << numFact << "! = " << fact << std::endl;
+
+
 	a.SetInteger("0");
 
 	BigInteger c;
@@ -53,7 +76,8 @@ int main()
 
 	a.LongMultiplicationToThis("99");
 
-	std::cout << a << std::endl;
+	//std::cout << a << std::endl;
+	//std::cout << fact << std::endl;
 	//std::cout << c << std::endl;
 	//std::cout << d << std::endl;
 
